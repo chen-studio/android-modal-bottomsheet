b6d9c04dd585381597faef980c8621516dab23b9
chen-studio <chenand35@gmail.com>
2024-12-26 15:47:00 +0900

    chore - Remove redundant files

---
+++ app/src/main/java/com/chen/studio/modal/bottomsheet/fragment/Half1StepBottomSheetFragment.kt
@@ -0,0 +1,7 @@
+package com.chen.studio.modal.bottomsheet.fragment
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class Half1StepBottomSheetFragment: BottomSheetDialogFragment() {
+
+}
